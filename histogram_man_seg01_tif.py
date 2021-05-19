2173e023bf138e84d50ca39cb5be30d73d5a70bc
Hannah L Winter <hannah.l.winter@gmail.com>
2021-05-19 10:26:49 +0200

    all files that work on other venv

---
+++ histogram_man_seg01_tif.py
@@ -0,0 +1,45 @@
+from PIL import Image
+import numpy
+from matplotlib import pyplot as plt
+#load image
+image = Image.open('man_seg01.tif')
+#make numpyarray
+imarray = numpy.array(image)
+#make onedimensional array
+imflat = imarray.flatten()
+
+#Make an array without zeros
+print(type(imflat))
+print(imflat.ndim)
+
+print(len(imflat))
+
+imflat_nozero = imflat[imflat != 0]
+
+print(len(imflat_nozero))
+#plot imflat
+
+#Wir haben diskrete Werte aufer x-Achse und keine kont. Werte, das schiebt alles schön hin
+d = numpy.diff(numpy.unique(imflat_nozero)).min()
+left_of_first_bin = imflat_nozero.min() - float(d)/2
+right_of_last_bin = imflat_nozero.max() + float(d)/2
+
+
+#make histogram
+plt.hist(imflat_nozero, numpy.arange(left_of_first_bin, right_of_last_bin + d, d), color = 'gray', ec = 'black' )
+#Titel
+plt.title('Man_seg01 - Erstes Histogramm',fontsize=15)
+#X-Achse Beschriftung
+plt.xlabel('Intensity')
+#y Achse Beschriftung
+plt.ylabel('Frequency')
+#customize number of x-ticks
+plt.xticks(range(1,25), fontsize = 7)
+
+
+plt.show()
+
+#plt.hist(imflat, bins=range(0,25))
+#plt.show()
+
+print(d)
---
+++ simple_histogram_dna_44.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import numpy
+from matplotlib import pyplot as plt
+
+# open image using Pillow
+# you probably have to alter the path to the image
+image44 = Image.open('dna-44.png')
+
+#make numpy array
+imarray44 = numpy.array(image44)
+
+#flatten the array
+imflat44 = imarray44.flatten()
+
+
+#We are using discrete values - so let's align..
+d = numpy.diff(numpy.unique(imflat44)).min()
+left_of_first_bin = imflat44.min() - float(d)/2
+right_of_last_bin = imflat44.max() + float(d)/2
+
+
+#Plot the histogram
+plt.hist(imflat44, numpy.arange(left_of_first_bin,right_of_last_bin+d,1), color = 'steelblue', ec = 'steelblue' )
+
+#customize x-ticks
+plt.xticks(numpy.arange(0,256,15), fontsize = 7)
+
+#title and label
+plt.title('Gray-value histogram')
+plt.xlabel('Intensity', fontsize = 9)
+plt.ylabel('Frequency', fontsize = 9)
+
+plt.show()
