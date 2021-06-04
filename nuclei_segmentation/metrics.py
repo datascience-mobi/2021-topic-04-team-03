6893bb5d4e2d97771ae7629546867ef72d9e06d1
Elizaveta Chernova <65237955+zyzllik@users.noreply.github.com>
2021-06-04 18:31:08 +0200

    Second half of cell counting algorithm

--- nuclei_segmentation/metrics.py
+++ nuclei_segmentation/metrics.py
@@ -7,6 +7,15 @@ def cell_counting (img):
             if 0 in img[(index[0]-1):(index[0]+2), (index[1]-1):(index[1]+2)]:
                 border_pixels.append(index)
 
-    return border_pixels
     # group border pixels
-    for pixel
+    pixel_groups = []
+    for start_pixel in border_pixels:
+        single_group = [start_pixel]
+        for other_pixel in border_pixels:
+            if other_pixel != start_pixel and math.dist(other_pixel, start_pixel) < 2:
+                single_group.append(other_pixel)
+                border_pixels.remove(other_pixel)
+        border_pixels.remove(start_pixel)
+        pixel_groups.append(single_group)
+
+    return pixel_groups
