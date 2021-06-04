26f3a0108eed6795693c61e69ba700ff84003e5d
Hannah L Winter <hannah.l.winter@gmail.com>
2021-06-04 20:27:41 +0200

    function that visualizes borders of the cells

--- nuclei_segmentation/visualisation.py
+++ nuclei_segmentation/visualisation.py
@@ -1,1 +1,18 @@
+import matplotlib.pyplot as plt
+
+def border_image(image, border_pixels, width):
+
+    half_width = width//2
+
+    border_image = image.copy()
+
+    for pixel in border_pixels:
+        for a in range(-half_width, half_width+1):
+            for b in range(-half_width, half_width+1):
+                if pixel[0] + a < border_image.shape[0] and pixel[1] + b < border_image.shape[1]:
+                    border_image[pixel[0] + a][pixel[1] + b] = 120
+
+    plt.imshow(border_image, 'PiYG')
+    plt.show()
+
 def overlay (test_image, gorund_thruth):
