9a92fe24765195e4ebde8e1cfddc3024e4ecc297
Hannah L Winter <hannah.l.winter@gmail.com>
2021-06-22 09:29:15 +0200

    paths corrected

--- nuclei_segmentation/__init__.py
+++ nuclei_segmentation/__init__.py
@@ -1,10 +1,10 @@
 import pathlib as pl
 
-N2DL_HeLa_im = [str(pl.Path(r'..\Data\N2DL-HeLa\img\*.tif'))]
-N2DL_HeLa_gt = [str(pl.Path(r'..\Data\N2DL-HeLa\gt\*.tif'))]
+N2DL_HeLa_im = [str(pl.Path('../Data/N2DL-HeLa/img/*.tif'))]
+N2DL_HeLa_gt = [str(pl.Path('../Data/N2DL-HeLa/gt/*.tif'))]
 
-NIH3T3_im = [str(pl.Path(r'..\Data\NIH3T3\img\*.png'))]
-NIH3T3_gt = [str(pl.Path(r'..\Data\NIH3T3\gt\*.png'))]
+NIH3T3_im = [str(pl.Path('../Data/NIH3T3/img/*.png'))]
+NIH3T3_gt = [str(pl.Path('../Data/NIH3T3/gt/*.png'))]
 
-N2DH_GOWT1_im = [str(pl.Path(r'..\Data\N2DH_GOWT1\img\*.tif'))]
-N2DH_GOWT1_gt = [str(pl.Path(r'..\Data\N2DH_GOWT1\gt\*.tif'))]
+N2DH_GOWT1_im = [str(pl.Path('../Data/N2DH_GOWT1/img/*.tif'))]
+N2DH_GOWT1_gt = [str(pl.Path(r'../Data/N2DH_GOWT1/gt/*.tif'))]
