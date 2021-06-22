71610a8e71a22a750479bbbb8c4738ed30af2c65
Laura-Waechter <82443892+Laura-Waechter@users.noreply.github.com>
2021-06-22 19:23:48 +0200

    values for msd hausdorff without preprocessing

--- Tests_and_Incompletes/data_without_preprocessing.py
+++ Tests_and_Incompletes/data_without_preprocessing.py
@@ -13,24 +13,24 @@ read_N2DL_HeLa_gt = imread_collection(N2DL_HeLa_gt)
 read_N2DL_HeLa_img = imread_collection(N2DL_HeLa_im)
 
 dice_list = []
-iou_list = []
 msd_list = []
+hausdorff_list = []
 
 for index in range(len(read_N2DL_HeLa_img)):
     plt.imshow(read_N2DL_HeLa_img[index], 'gray')
     plt.show()
     segmented_N2DL_HeLa = otsu.complete_segmentation(read_N2DL_HeLa_img[index], intensity_lvls=2 ** 16)
     dsc = evaluation.dice(segmented_N2DL_HeLa, read_N2DL_HeLa_gt[index])
-    iou = evaluation.iou(segmented_N2DL_HeLa, read_N2DL_HeLa_gt[index])
     msd = evaluation.msd(segmented_N2DL_HeLa, read_N2DL_HeLa_gt[index])
+    hausdorff = evaluation.hausdorff(segmented_N2DL_HeLa, read_N2DL_HeLa_gt[index])
 
     dice_list.append(dsc)
-    iou_list.append(iou)
     msd_list.append(msd)
+    hausdorff_list.append(hausdorff)
 
 print(dice_list)
-print(iou_list)
 print(msd_list)
+print(hausdorff_list)
 
 #dataset 1
 
@@ -41,24 +41,24 @@ read_N2DH_GOWT1_gt = imread_collection(N2DH_GOWT1_gt)
 read_N2DH_GOWT1_img = imread_collection(N2DH_GOWT1_im)
 
 dice_list = []
-iou_list = []
 msd_list = []
+hausdorff_list = []
 
 for index in range(len(read_N2DH_GOWT1_img)):
     plt.imshow(read_N2DH_GOWT1_img[index], 'gray')
     plt.show()
     segmented_N2DH_GOWT1 = otsu.complete_segmentation(read_N2DH_GOWT1_img[index], intensity_lvls=2 ** 16)
     dsc = evaluation.dice(segmented_N2DH_GOWT1, read_N2DH_GOWT1_gt[index])
-    iou = evaluation.iou(segmented_N2DH_GOWT1, read_N2DH_GOWT1_gt[index])
     msd = evaluation.msd(segmented_N2DH_GOWT1, read_N2DH_GOWT1_gt[index])
+    hausdorff = evaluation.hausdorff(segmented_N2DH_GOWT1, read_N2DH_GOWT1_gt[index])
 
     dice_list.append(dsc)
-    iou_list.append(iou)
     msd_list.append(msd)
+    hausdorff_list.append(hausdorff)
 
 print(dice_list)
-print(iou_list)
 print(msd_list)
+print(hausdorff_list)
 '''
 # dataset 3
 
@@ -69,21 +69,21 @@ read_NIH3T3_gt = imread_collection(NIH3T3_gt)
 read_NIH3T3_img = imread_collection(NIH3T3_im)
 
 dice_list = []
-iou_list = []
 msd_list = []
+hausdorff_list = []
 
 for index in range(len(read_NIH3T3_img)):
     plt.imshow(read_NIH3T3_img[index], 'gray')
     plt.show()
     segmented_NIH3T3 = otsu.complete_segmentation(read_NIH3T3_img[index], intensity_lvls=2 ** 16)
     dsc = evaluation.dice(segmented_NIH3T3, read_NIH3T3_gt[index])
-    iou = evaluation.iou(segmented_NIH3T3, read_NIH3T3_gt[index])
     msd = evaluation.msd(segmented_NIH3T3, read_NIH3T3_gt[index])
+    hausdorff = evaluation.hausdorff(segmented_NIH3T3, read_NIH3T3_gt[index])
 
     dice_list.append(dsc)
-    iou_list.append(iou)
     msd_list.append(msd)
+    hausdorff_list.append(hausdorff)
 
 print(dice_list)
-print(iou_list)
 print(msd_list)
+print(hausdorff_list)
