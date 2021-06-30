c13660c8db066dcebc2da7426123b7b9b6be5dc7
Laura-Waechter <82443892+Laura-Waechter@users.noreply.github.com>
2021-06-30 12:35:50 +0200

    deleted some files

--- Tests_and_Incompletes/plots.py
+++ Tests_and_Incompletes/plots.py
@@ -53,3 +53,21 @@ visualisation.comparison_swarmplot(dice_scores)
 msd_scores = np.array(msd_scores_NIH3T3)
 visualisation.comparison_swarmplot(msd_scores, y_label='MSD Value')
 
+# dataset 3 two-level Otsu
+
+with open("../Results/two_lvl.json", "r") as read_file:
+    data = json.load(read_file)
+
+dice_scores_NIH3T3_2 = []
+msd_scores_NIH3T3_2 = []
+
+for method in ["No preprocessing", "Median filter", "Gaussian filter", "Histogram stretching",
+               "Median filter and histogram stretching", "Gauss filter and histogram stretching"]:
+    dice_scores_NIH3T3.append(data[method]["NIH3T3"]["Dice Score"])
+    msd_scores_NIH3T3.append(data[method]["NIH3T3"]["MSD"])
+
+dice_scores = np.array(dice_scores_NIH3T3_2)
+visualisation.comparison_preprocessing(dice_scores)
+
+msd_scores = np.array(msd_scores_NIH3T3_2)
+visualisation.comparison_preprocessing(msd_scores, y_label='MSD Value')
